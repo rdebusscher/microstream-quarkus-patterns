72c76d8f77a7e86db374ff30cae5b5d062a66bca
Rudy De Busscher <rdebusscher@gmail.com>
2023-05-12 09:30:30 +0200

    Update to final version of MicroStream Quarkus extension

--- v2/plain/src/main/java/be/rubus/microstream/quarkus/example/controller/BookController.java
+++ v2/plain/src/main/java/be/rubus/microstream/quarkus/example/controller/BookController.java
@@ -7,9 +7,12 @@ import be.rubus.microstream.quarkus.example.service.BookService;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.Collection;
+import java.util.Optional;
 
 @Path("/book")
 @Produces(MediaType.APPLICATION_JSON)
@@ -22,6 +25,17 @@ public class BookController {
     @GET()
     public Collection<Book> getAll() {
         return bookService.getAll();
+    }
+
+    @GET()
+    @Path("{isbn}")
+    public Response getByIsbn(@PathParam("isbn") String isbn) {
+        Optional<Book> byISBN = bookService.getBookByISBN(isbn);
+        if (byISBN.isEmpty()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        } else {
+            return Response.ok().entity(byISBN.get()).build();
+        }
     }
 
 
